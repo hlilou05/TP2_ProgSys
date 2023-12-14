a0b7599d448889ce48b66ebc7c9cb736e35a43c6
mohamed hlilou <108653915+hlilou05@users.noreply.github.com>
2023-12-14 22:00:44 +0000

    Ex9

--- mini_memory.c
+++ mini_memory.c
@@ -1,3 +1,7 @@
+#include <cstddef>
 void* mini_calloc(int size_element,int number_element){
-    
+    void* mem = sbrk(size_element * number_element);
+    for (size_t i = 0; i < total_size; ++i) {
+        ((char*)mem)[i] = 0;
+    return mem;    
 }
